9fe8ae6ebaa21cb9a039a4874220e3517097f96d
Erick Gardin <erick@entre-quote.com>
2017-03-09 11:12:45 +0100

    Create php-default-object.php

---
+++ WP/php-default-object.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erick
+ * Date: 02/03/2017
+ * Time: 16:47
+ */
+ 
+namespace WonderWp\Theme\Components;
+
+class InsertComponent extends AbstractComponent
+{
+
+  private $title;
+  private $content;
+  
+  /* create getters and setters, !! specify fluent setters !! */
+  
+  public function getMarkup($opts = array())
+  {
+  
+    $markup = '';
+    return $markup;
+  }
+  
+}
